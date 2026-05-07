b806bfd9fc0f467f9227c2a0a975c6efb0001d0e
seawoong <seawoong@seawoongui-MacBookPro.local>
2026-05-07 23:29:36 +0900

    feat: Spring Boot 기본 세팅 완료

---
+++ src/main/java/com/heilous/HeilousApplication.java
@@ -0,0 +1,13 @@
+package com.heilous;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeilousApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeilousApplication.class, args);
+    }
+
+}
