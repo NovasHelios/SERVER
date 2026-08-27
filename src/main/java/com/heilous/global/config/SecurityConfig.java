59f01e10ebee51e1c56696814f6d2b34e63fbfde
QnpfqWhofr <dsmhwjh0627@dgsw.hs.kr>
2026-08-28 08:58:23 +0900

    auth fix

--- src/main/java/com/heilous/global/config/SecurityConfig.java
+++ src/main/java/com/heilous/global/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -50,10 +51,9 @@ public class SecurityConfig {
                                 "/swagger-ui/**",
                                 "/v3/api-docs/**",
                                 "/api/vworld/**",
-                                "/api/lands",
-                                "/api/lands/filter",
                                 "/uploads/**"
                         ).permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/lands","/api/lands/**").permitAll()
                         .anyRequest().authenticated()
                 )
 
