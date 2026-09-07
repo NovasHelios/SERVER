c79193a06add7cffe67d37311ef630a5caedb462
QnpfqWhofr <dsmhwjh0627@dgsw.hs.kr>
2026-09-07 20:06:45 +0900

    error message error fix jjinjjinmak

--- src/main/java/com/heilous/global/config/SecurityConfig.java
+++ src/main/java/com/heilous/global/config/SecurityConfig.java
@@ -6,7 +6,6 @@ import com.heilous.global.auth.JwtProvider;
 import com.heilous.global.auth.OAuth2SuccessHandler;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -18,7 +17,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.servlet.HandlerExceptionResolver;
 
 @Configuration
 @EnableWebSecurity
@@ -28,9 +26,6 @@ public class SecurityConfig {
     private final JwtProvider jwtProvider;
     private final CustomOAuth2UserService customOAuth2UserService;
     private final OAuth2SuccessHandler oAuth2SuccessHandler;
-
-    @Qualifier("handlerExceptionResolver")
-    private final HandlerExceptionResolver handlerExceptionResolver;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -96,7 +91,7 @@ public class SecurityConfig {
                 )
 
                 .addFilterBefore(
-                        new JwtAuthFilter(jwtProvider, handlerExceptionResolver),
+                        new JwtAuthFilter(jwtProvider),
                         UsernamePasswordAuthenticationFilter.class
                 );
 
